05659312442b729c0ce7374b2739b4e69c19a7e1
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-24 05:17:30 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,12 @@
 class StudentsController < ApplicationController
-    before_action :set_student, only: :show
-
+  before_action :set_student, only: :show
+  
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
-
 
   def new
   end
@@ -18,8 +16,9 @@ class StudentsController < ApplicationController
     redirect_to new_student_path
   end
 
+  private
+
     def set_student
       @student = Student.find(params[:id])
     end
-    
 end
